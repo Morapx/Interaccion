af1b3b0918163f208e86faffa4581dddbfecb630
Morapx <Bryan_alexis_1999@hotmail.com>
2019-09-19 08:28:47 -0700

    Interaccion end

--- Interaccion/MainWindow.xaml.cs
+++ Interaccion/MainWindow.xaml.cs
@@ -23,9 +23,28 @@ namespace Interaccion
         public MainWindow()
         {
             InitializeComponent();
+            btnboton.Click += Btnboton_Click;
+        }
+
+        private void Btnboton_Click(object sender, RoutedEventArgs e)
+        {
             lblHolaAmigos.Text = "Adios Amigos!";
             lblHolaAmigos.FontSize = 29;
             lblHolaAmigos.Foreground = Brushes.Aqua;
+        }
+
+        private void BtnbotonSegundo_Click(object sender, RoutedEventArgs e)
+        {
+            lblHolaAmigos.Text = "Adios segunda vez";
+            lblHolaAmigos.FontSize = 15;
+            lblHolaAmigos.Foreground = Brushes.BlueViolet;
+        }
+
+        private void BtnbotonTercero_Click(object sender, RoutedEventArgs e)
+        {
+            lblHolaAmigos.Text = "Adios tercera vez";
+            lblHolaAmigos.FontSize = 25;
+            lblHolaAmigos.Foreground = Brushes.DarkTurquoise;
         }
     }
 }
